7d435ee4e34bca7550b4c5c036d75c6a35dad39b
thZero.com <development@thzero.com>
2018-09-03 14:16:45 -0500

    master

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyProduct("thZero.NetCore.Library.Services.PasswordGenerator.MlkPwgen")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f2cd5aaa-a169-42db-8f69-49529d344632")]
---
+++ ServicePasswordGeneratorMlkPwgen.cs
@@ -0,0 +1,44 @@
+﻿/* ------------------------------------------------------------------------- *
+thZero.NetCore.Library.Services.Json.Newtonsoft
+Copyright (C) 2016-2018 thZero.com
+
+<development [at] thzero [dot] com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ * ------------------------------------------------------------------------- */
+
+using System;
+
+using MlkPwgen;
+
+namespace thZero.Services
+{
+	public sealed class ServicePasswordGeneratorMlkPwgen : IServicePasswordGenerator
+	{
+		#region Public Methods
+		public string Generate(int length, string allowed)
+        {
+            return PasswordGenerator.Generate(length, allowed);
+		}
+
+        public string GenerateAntiForgery(string name, int length = 15)
+        {
+            return string.Concat(name, Seperator, PasswordGenerator.Generate(length));
+        }
+        #endregion
+
+        #region Constants
+        private const string Seperator = ".";
+        #endregion
+    }
+}
